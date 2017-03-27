c28195b75cab4ed4cf161a73d7e3ca86f45d40db
Anthony Rebello <rebello.anthony@gmail.com>
2017-03-27 15:47:24 +0530

    fixes #52, stops iteration when client disconnects

--- funcserver/funcserver.py
+++ funcserver/funcserver.py
@@ -275,6 +275,22 @@ class CustomStaticFileHandler(StaticFileHandler):
 
 class RPCHandler(BaseHandler):
 
+    def __init__(self, *args, **kwargs):
+        super(RPCHandler, self).__init__(*args, **kwargs)
+
+        # stop_iteration is turned on when the connection closes
+        # or self.finish is called. This is a signal to stop
+        # the iteration of any generator that is writing to the client.
+        self.stop_iteration = False
+
+    def on_finish(self):
+        super(RPCHandler, self).on_finish()
+        self.stop_iteration = True
+
+    def on_connection_close(self):
+        super(RPCHandler, self).on_connection_close()
+        self.stop_iteration = True
+
     def _get_apifn(self, fn_name):
         obj = self.api
         for part in fn_name.split('.'):
@@ -392,13 +408,22 @@ class RPCHandler(BaseHandler):
             self.write(r)
             return
 
-        # Streaming response - iterate and write out
-        for part in result:
+        # Iterating over the results until iteration completes or
+        # the connection is closed.
+        while not self.stop_iteration:
+            try:
+                part = next(result)
+            except StopIteration:
+                break
+
             part = serializer(part)
             self.write(part)
             sep = '\n' if is_raw else self.get_record_separator(protocol)
             if sep: self.write(sep)
             self.flush()
+
+        del result
+        gc.collect()
 
     def get_record_separator(self, protocol):
         return {'msgpack': '',
