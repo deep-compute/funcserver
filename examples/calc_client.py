274c7d3463836104ba2ac7aa5c6a64a74b5e560a
rajendra <rajendra@deepcompute.com>
2019-07-31 08:24:46 +0200

    reformat with fast

--- examples/calc_client.py
+++ examples/calc_client.py
@@ -1,11 +1,13 @@
 from funcserver import Client
 
+
 def main():
-    c = Client('http://localhost:8889')
+    c = Client("http://localhost:8889")
     print c.add(10, 20)
     print c.sub(10, 20)
     print c.mul(10, 20)
     print c.div(10, 20.0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
